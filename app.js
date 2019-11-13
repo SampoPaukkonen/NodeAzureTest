08ed02c65eba8f0eb5c9606af96459eb9c5c72bf
Sampo Paukkonen <sampopaukkonen@localhost.localdomain>
2019-11-13 20:11:12 +0200

    second commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
+    res.end('<h3>Junction, wooo!</h3>');
     });
 
 server.listen(port, () => {
