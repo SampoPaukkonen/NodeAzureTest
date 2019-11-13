deeed2323029b115fe0d3a01325beed08ee5dd4b
Sampo Paukkonen <sampopaukkonen@localhost.localdomain>
2019-11-13 20:39:24 +0200

    Nth commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<h3>Afdasljlkfjsdalkfjlaskflk</h3>');
+    res.end('<h3>Himo burger tietoteekkari</h3>');
     });
 
 server.listen(port, () => {
